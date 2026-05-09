359a475c38d01b925e7fe5ded7d0024114cb3995
Your Name <psemsapp@gmail.com>
2026-05-09 23:41:13 +0800

    fixed resume link to local pdf

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -9,9 +9,6 @@ export const personalData = {
   github: 'https://github.com/psemsapp-cell',
   facebook: 'https://www.facebook.com/jamespalencia030604',
   linkedIn: 'https://www.linkedin.com/in/alcon-james-palencia-167947401/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  devUsername: "psemsapp-cell",
+  resume: '/resume.pdf',
 }
